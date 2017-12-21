a9b74162ac96b15cca54d22f18335d5c6d15073c
veekxt <veekxt@gmail.com>
2017-12-21 14:22:51 +0800

    add register.html

--- main.py
+++ main.py
@@ -4,23 +4,30 @@ from flask import Flask, request, render_template
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import Required
+from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 
 import config
 
+
 class LoginForm(FlaskForm):
-    email = StringField(validators=[Required()])
-    password = PasswordField(validators=[Required()])
+    email = StringField(validators=[DataRequired()])
+    password = PasswordField(validators=[DataRequired()])
     submit = SubmitField()
 
+
 class RegisterForm(FlaskForm):
-    pass
+    email = StringField(validators=[DataRequired()])
+    password_1 = PasswordField(validators=[DataRequired()])
+    password_2 = PasswordField(validators=[DataRequired()])
+    submit = SubmitField()
+
 
 app = Flask(__name__)
 app.config.from_object(config)
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
+
 
 class User(db.Model):
     __table_name__ = 'user'
@@ -29,6 +36,7 @@ class User(db.Model):
     email = db.Column(db.Text)
     name = db.Column(db.Text)
 
+
 class Article(db.Model):
     __table_name__ = 'article'
     id = db.Column(db.Integer, primary_key=True)
@@ -36,16 +44,19 @@ class Article(db.Model):
     title = db.Column(db.Text)
     text = db.Column(db.Text)
 
+
 @app.route('/data_test')
 def data_test():
     article = Article(time='1999-01-08 04:05:06', title="test_title", text="test_text")
     db.session.add(article)
     db.session.commit()
-    return str(article.id)+'++'+str(article.title)+'++'+str(article.text)
+    return str(article.id) + '++' + str(article.title) + '++' + str(article.text)
+
 
 @app.route('/')
 def index():
     return render_template('index.html', text='Hello, My Site!')
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def user_login():
@@ -54,9 +65,14 @@ def user_login():
         pass
     return render_template('login.html', form=form)
 
+
 @app.route('/register', methods=['GET', 'POST'])
 def user_register():
     form = RegisterForm()
+    if form.validate_on_submit():
+        pass
+    return render_template('register.html', form=form)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
