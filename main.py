74b4e03866e3d5ed270ea39aafac5129133aed2f
veekxt <veekxt@gmail.com>
2018-08-12 23:27:11 +0800

    语法高亮&后端渲染,close #13

--- main.py
+++ main.py
@@ -1,28 +1,19 @@
 #!/usr/bin/env python3
-#encoding:utf-8
+# encoding:utf-8
 import os
-import time
-
 from datetime import datetime
 from flask import Flask, request, render_template, url_for, flash, Blueprint
 from flask_bootstrap import Bootstrap
-from flask_wtf import FlaskForm
 from sqlalchemy import desc
 from werkzeug.utils import secure_filename, redirect
-from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired
-from flask_sqlalchemy import SQLAlchemy
-from flask_mail import Message, Mail
-from flask_login import LoginManager
 import json
 from flask_login import current_user, login_required
-
-from my_utils import my_secure_filename,hgihtlight_word
+from my_utils import my_secure_filename, hgihtlight_word
 from data_model import db, Article, User, Tag
 from auth import login_manager
-
 import config_t
 from auth import auth as auth_blueprint
+import CommonMark
 
 app = Flask(__name__)
 app.config.from_object(config_t)
@@ -32,8 +23,9 @@ login_manager.init_app(app)
 app.register_blueprint(auth_blueprint)
 app.jinja_env.globals.update(hgihtlight_word=hgihtlight_word)
 
-#@app.route("/utils")
-#def utils():
+
+# @app.route("/utils")
+# def utils():
 #    tag = Tag.query.all()
 #    for a in tag:
 #        print(a.name)2
@@ -52,17 +44,20 @@ def get_tag_dict():
         tag_dict[tag.name] = (tag.id, len(tag.articles.all()))
     return tag_dict
 
+
 def root_required(func):
     '''
     装饰器，有些操作只有我能执行，验证current_user为管理员
     '''
+
     def access_is_0(*args, **kwargs):
         if current_user.access == 0:
-            func(*args,**kwargs)
+            func(*args, **kwargs)
         else:
             # flash("You have no permission to access this page")
             # TODO: diffrent call from diffrent path
             return "ERROR PERMISSION"
+
     return access_is_0
 
 
@@ -81,7 +76,9 @@ def index():
                                                                          per_page=app.config['POSTS_PER_PAGE'],
                                                                          error_out=True)
     articles = pagination.items
-    return render_template('index.html', articles=articles, pagination=pagination, tag_dict=get_tag_dict(), args=request.args)
+    return render_template('index.html', articles=articles, pagination=pagination, tag_dict=get_tag_dict(),
+                           args=request.args)
+
 
 @app.route('/up', methods=['GET', 'POST'])
 def my_upload():
@@ -110,7 +107,7 @@ def post_article():
         # flash("当前用户没有权限发布文章！")
         return "Cant access!"
     try:
-        article_info = json.loads(str(request.get_data(),encoding="utf-8"))
+        article_info = json.loads(str(request.get_data(), encoding="utf-8"))
         article = Article(
             author=current_user.id,
             time=datetime.now(),
@@ -153,7 +150,21 @@ def select_tags():
 
 @app.route("/article/<id>")
 def a_article(id):
-    return render_template("article.html", id=id, tag_dict=get_tag_dict())
+    article = Article.query.get(int(id))
+    if not article: app.abort(404)
+    parser = CommonMark.Parser()
+    ast = parser.parse(article.text)
+    renderer = CommonMark.HtmlRenderer()
+    html = renderer.render(ast)
+
+    article_dict = {
+        "title": article.title,
+        "time": str(article.time),
+        "text": html,
+        "author": article.user.name
+    }
+
+    return render_template("article.html", id=id, tag_dict=get_tag_dict(), article_dict=article_dict)
 
 
 @app.route("/article_info/<id>")
@@ -167,6 +178,7 @@ def a_article_info(id):
     }
     return json.dumps(article_json)
 
+
 @app.route("/article/delete/<id>")
 def del_article(id):
     if current_user.access != 0:
@@ -177,23 +189,27 @@ def del_article(id):
     flash("已删除一篇文章！")
     return redirect(url_for("index"))
 
+
 @app.route("/search")
 def search_article():
     k = request.args.get("keyword")
-    if len(k)==0:
+    if len(k) == 0:
         return redirect(url_for("index"))
-    ar = Article.query.filter(Article.title.like("%"+k+"%"))
-    pagination = ar.order_by(desc(Article.time)).paginate(1,per_page=1024,error_out=True)
+    ar = Article.query.filter(Article.title.like("%" + k + "%"))
+    pagination = ar.order_by(desc(Article.time)).paginate(1, per_page=1024, error_out=True)
     return render_template('index.html', articles=pagination.items, pagination=pagination, tag_dict=get_tag_dict(),
                            args=request.args, search_keyword=k)
 
+
 @app.route("/utils")
 def utils_src():
-    return redirect(url_for("utils",wh="tetris"))
+    return redirect(url_for("utils", wh="tetris"))
+
 
 @app.route("/utils/<wh>")
 def utils(wh):
-    return render_template(wh+".html",util_id=wh)
+    return render_template(wh + ".html", util_id=wh)
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8000, debug=True)
